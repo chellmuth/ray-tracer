f2063fdea65deacb05ddc90e1dc54ac09aadf08d
Chris Hellmuth <chellmuth@gmail.com>
2015-08-05 21:16:12 -0600

    Grid doesn't support transforms yet, so leave object at origin and move camera

--- world.py
+++ world.py
@@ -18,42 +18,23 @@ from vector import Vector3
 class World(object):
     def __init__(self):
         self.tracer = Tracer(self)
-        self.sampler = Sampler(25)
+        self.sampler = Sampler(1)
         self.view_plane = ViewPlane()
         self.background_color = RGBColor(0.1, 0.1, 0.1)
         self.camera = Camera()
         self.geometry = []
         self.data = numpy.zeros((self.view_plane.vres, self.view_plane.hres, 3), dtype=numpy.uint8)
 
-        sphere1 = Instance(Sphere(Point3(0.0, 0.0, 0.0), 2.0, Phong(RGBColor.Red())))
-        sphere1.scale(1.0, 1.5, 1.0)
-        sphere1.translate(3.0, 2.0, -22.0)
-        self.add_geometry(sphere1)
-
-        disk = Instance(Disk(
-            Point3(0.0, 0.0, 0.0),
-            Point3(0.0, 0.0, 1.0).normalized(),
-            30,
-            Matte(RGBColor(0.5, 0.9, 0.4))
-        ))
-        disk.rotate_x(-math.pi / 2)
-        disk.translate(0.0, 5.0, -45.0)
-        self.add_geometry(disk)
-
-        cylinder = Instance(OpenCylinder(center=Point3(0.0, 0.0, 0.0), radius=3.0, material=Phong(RGBColor(1.0, 1.0, 0.5))))
-        cylinder.translate(-4.0, 0.0, -36.0)
-        self.add_geometry(cylinder)
-
         mesh = load_obj("cube.obj")
         for face in mesh.faces:
-            triangle = Instance(FlatMeshTriangle(mesh, face, material=Phong(RGBColor.Blue())))
-            triangle.scale(3.5, 3.5, 3.5)
-            triangle.rotate_y(-math.pi / 10)
-            triangle.translate(-1.3, 3.5, -25.0)
+            triangle = FlatMeshTriangle(mesh, face, material=Phong(RGBColor.Blue()))
+            # triangle.scale(3.5, 3.5, 3.5)
+            # triangle.rotate_y(-math.pi / 10)
+            # triangle.translate(-1.3, 3.5, -25.0)
             self.add_geometry(triangle)
 
         self.ambient_light = AmbientLight()
-        self.lights = [ PointLight(Point3(8.0, 2.0, -5.0)) ]
+        self.lights = [ PointLight(Point3(8.0, 2.0, 5.0)) ]
 
     def add_geometry(self, geometry):
         self.geometry.append(geometry)
